199e68e8917809ca914096751d71175b7d227404
Josh Kamdjou <jkamdjou@gmail.com>
2021-04-28 19:42:26 -0400

    address time lag bug

--- aws/create_and_upload_self_signed_cert_acm.sh
+++ aws/create_and_upload_self_signed_cert_acm.sh
@@ -13,11 +13,21 @@ if [[ -z $CERT_EXISTS_ALREADY || $CERT_EXISTS_ALREADY == "placeholder" ]]
 then
     echo "Creating ACM and SSM parameter for ACM certificate"
 
+    # there can be up to 15 minutes of time lag between the EC2 instance
+    # and ACM. to avoid issues with "certs in the future" according to ACM,
+    # we reset the date briefly by 2 days, generate the cert, then restart the
+    # time daemon to reset the clock back to normal
+    CERT_START_DATE=$(date -d "$date -2 days" +"%y%m%d")
+    sudo systemctl stop systemd-timesyncd
+    sudo date -s $CERT_START_DATE
+
     openssl genrsa -out platform-ca.key 2048
     openssl req -x509 -new -nodes -key platform-ca.key -sha256 -days 800 -out platform-ca.crt -subj "/C=US/O=Sublime Security/OU=Sublime Platform"
     openssl genrsa -out platform.key 2048
     openssl req -new -key platform.key -config self_signed_cert_config.cnf -out platform.csr
     openssl x509 -req -in platform.csr -CA platform-ca.crt -CAkey platform-ca.key -CAcreateserial -out platform.crt -days 800 -sha256 -extfile self_signed_cert_config.cnf -extensions req_ext
+
+    sudo systemctl start systemd-timesyncd
 
     CERT_ARN="$(aws acm import-certificate --certificate fileb://platform.crt --private-key fileb://platform.key --certificate-chain fileb://platform-ca.crt --region $AWS_REGION --tags '[{"Key":"Function", "Value": "Self-signed certificate for Sublime Security on-prem deployment"}]' | jq -r .CertificateArn)"
     echo "CERT_ARN: $CERT_ARN"
